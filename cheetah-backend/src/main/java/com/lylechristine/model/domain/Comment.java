aca2f494971c1376e39e9c8fd156c9a87a164a89
Lylio <cxz@gmx.com>
2022-12-12 20:29:52 +0000

    feat : add commit

--- cheetah-backend/src/main/java/com/lylechristine/model/domain/Comment.java
+++ cheetah-backend/src/main/java/com/lylechristine/model/domain/Comment.java
@@ -1,7 +1,48 @@
 package com.lylechristine.model.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 /**
  * @author Lyle Christine
  */
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private long id;
+
+    @Column(name = "body", columnDefinition = "TEXT")
+    private String body;
+
+    @CreatedDate
+    @NotNull
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "post_id", nullable = false)
+    @NotNull
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    @NotNull
+    private User user;
 }
