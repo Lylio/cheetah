dd2b777fe4c895b1365aeeb386907154c1b3c94b
Lylio <cxz@gmx.com>
2023-01-05 23:24:13 +0000

    feat : create login and signup requests

--- cheetah-backend/src/main/java/com/lylechristine/model/request/LoginRequest.java
+++ cheetah-backend/src/main/java/com/lylechristine/model/request/LoginRequest.java
@@ -1,7 +1,37 @@
 package com.lylechristine.model.request;
 
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author Lyle Christine
  */
+
+@ToString
 public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
