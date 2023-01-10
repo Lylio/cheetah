84f976745424dde8f5b4934474dcdc4144b1bb70
Lylio <cxz@gmx.com>
2023-01-10 22:58:03 +0000

    feat : add AuthResponse

--- cheetah-backend/src/main/java/com/lylechristine/model/response/AuthResponse.java
+++ cheetah-backend/src/main/java/com/lylechristine/model/response/AuthResponse.java
@@ -4,4 +4,27 @@ package com.lylechristine.model.response;
  * @author Lyle Christine
  */
 public class AuthResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 }
